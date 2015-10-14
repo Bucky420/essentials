6f2ece2a6d68d095c346d2bafc5b50c14728f8a7
codydbgt <codydbgt@gmail.com>
2015-10-14 00:46:52 -0400

    :)

---
+++ essentials/src/ntg/codydbgt/essentials/BukkitPlugin.java
@@ -0,0 +1,12 @@
+package ntg.codydbgt.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		//were done here :) 
+	}
+
+}
